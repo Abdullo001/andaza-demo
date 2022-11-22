405e1351aadd4f7b3aafb41cdfffe79d53ea716c
Abbos Kahamidullaev <khamidullaev1998@gmail.com>
2022-11-23 00:16:06 +0500

    created login page

--- pages/index.vue
+++ pages/index.vue
@@ -6,7 +6,29 @@
           <img src="@/assets/images/login.png" alt="login image">
         </div>
       </v-col>
-      <v-col></v-col>
+      <v-col class="d-flex justify-center align-center">
+        <div class="login__main">
+          <div class="login__logo">
+            <img src="/logo.svg" alt="logo">
+          </div>
+          <div class="login__description">Registration</div>
+          <v-text-field
+            label="Company name"
+            filled
+            dense
+            color="#7631FF"
+            placeholder="Enter Company name"
+            class="mb-3"
+          />
+          <v-text-field
+            label="Username"
+            filled
+            dense
+            color="#7631FF"
+            placeholder="Enter Username or E-mail"
+          />
+        </div>
+      </v-col>
     </v-row>
   </div>
 </template>
@@ -17,6 +39,4 @@ export default {
   layout: 'main'
 }
 </script>
-<style lang="scss" src="assets/_global.scss">
-
-</style>
+<style lang="scss" src="assets/abstracts/_login.scss"></style>
