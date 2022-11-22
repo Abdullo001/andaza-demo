3ca289f3a5e652e7d16111596f710e5e011395a8
Abbos Kahamidullaev <khamidullaev1998@gmail.com>
2022-11-22 23:13:56 +0500

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="login">
+    <v-row>
+      <v-col>
+        <div class="login__image">
+          <img src="@/assets/images/login.png" alt="login image">
+        </div>
+      </v-col>
+      <v-col></v-col>
+    </v-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+  layout: 'main'
+}
+</script>
+<style lang="scss" src="assets/_global.scss">
+
+</style>
