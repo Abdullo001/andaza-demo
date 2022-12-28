94378885d2ee0c92af3db41d845cc91d7d8311c8
Abbos Kahamidullaev <khamidullaev1998@gmail.com>
2022-12-28 09:51:07 +0500

    add Documents icons

--- pages/prefinances/_id.vue
+++ pages/prefinances/_id.vue
@@ -1,5 +1,13 @@
 <template>
   <div>
+    <div class="breadcrumbs d-flex align-center ml-2">
+      <div class="breadcrumbs d-flex align-center font-weight-medium mb-4 text-body-2" v-for="(item,idx) in map_links"
+           :key="idx">
+        <nuxt-link :to="item.to" class="base-color" v-if="!item.disabled">{{ item.text }}</nuxt-link>
+        <div class="grey--text" v-if="item.disabled">{{ item.text }}</div>
+        <v-icon class="mx-3" size="18" v-if="item.icon">mdi-slash-forward</v-icon>
+      </div>
+    </div>
     <v-card elevation="0" class="mt-2 rounded-lg">
       <v-card-title>
         <div>Add prefinance</div>
@@ -319,6 +327,26 @@
 export default {
   data() {
     return {
+      map_links: [
+        {
+          text: 'Home',
+          disabled: false,
+          to: '/',
+          icon: true
+        },
+        {
+          text: 'Prefinances',
+          disabled: false,
+          to: '/prefinances',
+          icon: true
+        },
+        {
+          text: 'Details',
+          disabled: true,
+          to: '/user-management/7a42ec47-7351-4128-9db9-5236adbbfe6d',
+          icon: false
+        },
+      ],
       refinances: {
         prefinanceNumber: '',
         modelId: '',
