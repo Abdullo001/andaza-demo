8033b92739b094ecd6788546d2ea6409e68e06f0
Abbos Kahamidullaev <khamidullaev1998@gmail.com>
2023-01-16 19:29:48 +0500

    add filters on fraud devices

--- store/fraud.js
+++ store/fraud.js
@@ -22,5 +22,40 @@ export const actions = {
     await this.$axios.$put('/api/v1/fraud-management/blocked-devices', body)
       .then(res => commit('setDevices', res.data))
       .catch(({response}) => console.log(response))
+  },
+  filterDevice({commit}, {deviceId, deviceNumber, status}) {
+    const body = {
+      filters: [
+        {
+          key: 'deviceId',
+          operator: 'LIKE',
+          propertyType: 'STRING',
+          value: deviceId
+        },
+        {
+          key: 'blockedDeviceId',
+          operator: 'LIKE',
+          propertyType: 'STRING',
+          value: deviceNumber
+        },
+        {
+          key: 'status',
+          operator: 'LIKE',
+          propertyType: 'STRING',
+          value: status
+        },
+      ],
+      sort: [],
+      page: 0,
+      size: 10
+    }
+    body.filters = body.filters.filter(item => item.value !== '' && item.value !== null)
+    this.$axios.$put('/api/v1/fraud-management/blocked-devices', body)
+      .then(res => {
+        console.log(res);
+      })
+      .catch(({response}) => {
+        console.log(response);
+      })
   }
 }
