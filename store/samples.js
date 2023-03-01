6bd1b7b05b61ca615c225472e761704c1ca4dd15
Abbos Kahamidullaev <khamidullaev1998@gmail.com>
2023-03-01 15:46:54 +0500

    samples tabs make is CRUD

--- store/samples.js
+++ store/samples.js
@@ -1,7 +1,6 @@
 export const state = () => ({
   samplesList: [],
   purposesEnums: [],
-
 });
 export const getters = {
   samplesList: state => state.samplesList,
@@ -20,13 +19,14 @@ export const actions = {
   getSamplesList({commit}, modelId) {
     this.$axios.$get(`/api/v1/samples/list-by-model?modelId=${modelId}`)
       .then(res => {
-        console.log(res);
+        commit('setSamples', res.data)
       }).catch(({response}) => console.log(response))
   },
-  createSamples({dispatch}, data) {
-    this.$axios.$post('/api/v1/samples/create', data)
+  async createSamples({dispatch}, data) {
+    await this.$axios.$post('/api/v1/samples/create', data)
       .then(res => {
-        console.log(res);
+        this.$toast.success(res.message)
+        dispatch('getSamplesList', data.modelId);
       }).catch(({response}) => console.log(response))
   },
   getPurposesEnums({commit}) {
@@ -39,6 +39,20 @@ export const actions = {
     this.$axios.$put(`/api/v1/sample-purposes/list`, body)
       .then(res => {
         commit('setPurposesEnums', res.data.content)
+      }).catch(({response}) => console.log(response))
+  },
+  deleteSamples({dispatch}, {modelId, elemId}) {
+    this.$axios.$delete(`/api/v1/samples/delete?id=${elemId}`)
+      .then(res => {
+        dispatch('getSamplesList', modelId);
+        console.log(res);
+      }).catch(({response}) => console.log(response))
+  },
+  async updateSamples({dispatch}, data) {
+    await this.$axios.$put(`/api/v1/samples/update`, data)
+      .then(res => {
+        dispatch('getSamplesList', data.modelId);
+        this.$toast.success(res.message);
       }).catch(({response}) => console.log(response))
   }
 
