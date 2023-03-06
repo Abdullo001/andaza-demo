7f110c715607affacef883164d8a5dd66d1b17f7
Abbos Kahamidullaev <khamidullaev1998@gmail.com>
2023-03-06 14:27:41 +0500

    General do CRUD

--- store/instruction.js
+++ store/instruction.js
@@ -1,13 +1,44 @@
-export const state = () => ({});
-export const getters = {};
-export const mutations = {};
+export const state = () => ({
+  commentText: {}
+});
+export const getters = {
+  commentText: state => state.commentText,
+
+};
+export const mutations = {
+  setCommentText(state, comment) {
+    state.commentText = comment;
+  }
+};
 export const actions = {
-  uploadImages({commit}, {data, modelId}) {
+  uploadImages({commit}, {data}) {
     this.$axios.$post(`/api/v1/instructions/create`, data)
       .then(res => {
         this.$toast.success(res.message)
       }).catch(({response}) => {
       this.$toast.error(response.data.message)
     })
+  },
+  async getComment({commit}, {modelId, type}) {
+    await this.$axios.$get(`/api/v1/instructions/get?modelId=${modelId}&type=${type}`)
+      .then(res => {
+        commit('setCommentText', res.data)
+      }).catch(({response}) => console.log(response))
+  },
+  async createComments({commit}, data) {
+    delete data.id
+    await this.$axios.$post('/api/v1/instructions/create', data)
+      .then(res => {
+        this.$toast.success(res.message)
+        commit('setCommentText', res.data);
+      }).catch(({response}) => {
+        this.$toast.error(response.data.message)
+    })
+  },
+  updateComment({commit}, data) {
+    this.$axios.$put('/api/v1/instructions/update', data)
+      .then(res => {
+        this.$toast.success(res.message)
+      }).catch(({response}) => console.log(response))
   }
 };
