d57de8066c15e682bb21cf3ecf2eb9b3a55a4278
Valijon Mizrofov <valijonmizrofov7236@gmail.com>
2023-03-07 10:14:46 +0500

    commit permission

--- store/permission.js
+++ store/permission.js
@@ -3,11 +3,16 @@ export const state = () => ({
   permissions: [],
   permissionIdDAta: [],
   role: [],
+  roleOne: {}
 })
 export const getters = {
   loading: state => state.loading,
-  permissions: state => state.permissions,
-  role: state => state.role,
+  permissions: state => state.permissions.content,
+  totalElement: state => state.permissions.totalElements,
+  permissionIdDAta: state => state.permissionIdDAta,
+  role: state => state.role.content,
+  roleTotalElements: state => state.role.totalElements,
+  roleOne: state => state.roleOne,
 }
 export const mutations = {
   // PERMISSION
@@ -24,67 +29,76 @@ export const mutations = {
   setAllRole(state, data) {
     state.role = data
   },
+  setOneIdData(state, data){
+    state.roleOne = data
+  },
 }
 export const actions = {
   // GET ROLE IDENTIFIER
-  getRoleIdentifier({commit}, id){
-    this.$axios.get(`/api/v1/role/get?roleId=${id}`)
+  async getRoleIdentifier({commit}, id){
+    await this.$axios.get(`/api/v1/role/get?roleId=${id}`)
       .then(res => {
-        console.log(res)
+        console.log(res.data.data)
+        commit("setOneIdData", res.data.data)
+        this.$toast.success(res.data.message)
+
       })
       .catch(({response}) => {
         console.log(response)
       })
   },
-  // GET ROLE
-  getRoleAllData({commit}, {page, size}) {
+  // GET ROLE ALL DATA
+  async getRoleAllData({commit}, {page, size}) {
     const body = {
       filters: [],
       sort: [],
       page: page,
       size: size
     }
-    this.$axios.$put('/api/v1/permission/list', body)
+    await this.$axios.$put('/api/v1/role/list', body)
       .then(res => {
-        console.log(res)
         commit('setLoading', false);
+        commit("setAllRole", res.data)
       })
       .catch(response =>  {
         console.log(response)
       })
   },
   // POST ROLE
-  postRole(context, data) {
-    this.$axios.$post('/api/v1/permission/list', data)
+  async postRole(context, data) {
+    await this.$axios.$post('/api/v1/role/create', data)
       .then(res => {
-        console.log(res)
         context.commit('setLoading', false)
+        context.dispatch("getRoleAllData", {page: 0, size: 10})
+        this.$toast.success(res.message)
       })
       .catch(({response}) => {
         console.log(response)
       })
   },
   // UPDATE ROLE
-  updateRole(context, data) {
-    this.$axios.$put(' /api/v1/role/update')
+  async updateRole(context, data) {
+    await this.$axios.$put('/api/v1/role/update', data)
       .then(res => {
-        console.log(res)
+        context.dispatch("getRoleAllData", {page: 0, size: 10})
+        this.$toast.success(res.message)
       })
       .catch(({response}) => {
         console.log(response)
       })
   },
   // GET PERMISSION
-  getPermission({commit}, {page, size}) {
+  async getPermission({commit}, {page, size}) {
     const body = {
       filters: [],
       sort: [],
       page: page,
       size: size
     }
-    this.$axios.$put('/api/v1/pre-finances/list', body)
+    await this.$axios.$put('/api/v1/permission/list', body)
       .then(res => {
         commit('setLoading', false)
+        commit('setAllPermission', res.data)
       })
       .catch(({response}) => {
         commit('setLoading', false)
@@ -92,43 +106,68 @@ export const actions = {
       })
   },
   // POST PERMISSION
-  postPermission({context}, data) {
-    this.$axios.$post('/api/v1/permission/create', data)
+  async postPermission(context, data) {
+    await this.$axios.$post('/api/v1/permission/create', data)
       .then(res => {
-        console.log(res)
+        this.$toast.success(res.message, {theme: "toasted-primary"})
+        context.dispatch("getPermission", {page: 0, size: 10})
       })
       .catch(({response}) => {
-        this.$toast.error(response.errorMessages, {theme: 'toasted-primary'})
+        console.log(response)
       })
   },
   // UPDATE PERMISSION
-  putPermission({commit}, data) {
-    this.$axios.$post('/api/v1/permission/update', data)
+  async putPermission({dispatch}, data) {
+    await this.$axios.$put('/api/v1/permission/update', data)
       .then(res => {
-        console.log(res)
         this.$toast.success(res.message, {theme: 'toasted-primary'})
+        dispatch("getPermission", {page: 0, size: 10})
       })
       .catch(({response}) => {
         this.$toast.error(response.errorMessages, {theme: 'toasted-primary'})
       })
   },
   // FILTER DATA
-  filterData(context, {key, operator, property_type, value, value_to}) {
+  async filterData(context, {key, status, property_type, value, value_to}) {
     const body = {
       filters: [
         {
-          key: key,
-          operator: operator,
-          property_type: property_type,
+          key: "key",
+          operator: "LIKE",
+          property_type: "STRING",
+          value: key,
+        },
+        {
+          key: "status",
+          operator: "LIKE",
+          property_type: "STRING",
+          value: status,
+        },
+        {
+          key: "property_type",
+          operator: "LIKE",
+          property_type: "STRING",
+          value: property_type,
+        },
+        {
+          key: "value",
+          operator: "LIKE",
+          property_type: "STRING",
           value: value,
-          value_to: value_to,
-        }
+        },
+        {
+          key: "value_to",
+          operator: "LIKE",
+          property_type: "STRING",
+          value: value_to,
+        },
       ],
       sort: [],
       size: 10,
       page: 0,
     };
-    this.$axios.$put(`/api/v1/permission/list`, body)
+    body.filters = body.filters.filter(item => item.value !== '' && item.value !== null)
+    await this.$axios.$put(`/api/v1/permission/list`, body)
       .then(res => {
         this.$toast.success(res.message, {theme: 'toasted-primary'})
         context.commit('setAllPermission', res.data)
@@ -149,8 +188,8 @@ export const actions = {
       data: [],
     })
       .then(res => {
-        this.$toast.success(res.status, {theme: 'toasted-primary'});
-        commit('setPermissionID', res.data)
+        this.$toast.success(res.data.message, {theme: 'toasted-primary'});
+        commit('setPermissionID', res.data.data)
       })
       .catch(({response}) => {
         this.$toast.error(response.status, {theme: 'toasted-primary'})
