06b22cdc4f9a6d1e71334d992f98a09efbb60f70
Valijon Mizrofov <valijonmizrofov7236@gmail.com>
2023-04-05 12:17:55 +0500

    finish access control pages localization

--- pages/permission/index.vue
+++ pages/permission/index.vue
@@ -9,7 +9,7 @@
         <v-row class="mx-0 px-0 mb-2 mt-4 pa-4 w-full" justify="center">
           <v-col cols="12" lg="2" md="2">
             <v-text-field
-              label="Permission name"
+              :label="$t('permissionControl.dialog.permissionName')"
               outlined
               v-model="filter_search.key"
               class="rounded-lg"
@@ -20,7 +20,7 @@
           </v-col>
           <v-col cols="12" lg="2" md="2">
             <v-select
-              label="Permission path"
+              :label="$t('permissionControl.dialog.permissionPath')"
               outlined
               v-model="filter_search.property_type"
               class="rounded-lg"
@@ -33,7 +33,7 @@
           </v-col>
           <v-col cols="12" lg="2" md="2">
             <v-select
-              label="Status"
+              :label="$t('permissionControl.dialog.status')"
               outlined
               :items="statusEnums"
               v-model="filter_search.status"
@@ -50,7 +50,7 @@
             <el-date-picker
               type="datetime"
               v-model="filter_search.value"
-              placeholder="From"
+              :placeholder="$t('from')"
               :picker-options="pickerShortcuts"
               value-format="dd.MM.yyyy HH:mm:ss"
             >
@@ -61,7 +61,7 @@
           >
             <el-date-picker
               type="datetime"
-              placeholder="To"
+              :placeholder="$t('to')"
               v-model="filter_search.value_to"
               :picker-options="pickerShortcuts"
               value-format="dd.MM.yyyy HH:mm:ss"
@@ -76,7 +76,7 @@
                 class="text-capitalize mr-4 rounded-lg font-weight-bold"
                 @click.stop="resetSearch"
               >
-                Reset
+                {{ $t('permissionControl.dialog.reset') }}
               </v-btn>
               <v-btn
                 width="140" color="#397CFD" dark
@@ -84,7 +84,7 @@
                 class="text-capitalize rounded-lg font-weight-bold"
                 @click="filterPermission"
               >
-                Search
+                {{ $t('permissionControl.dialog.search') }}
               </v-btn>
             </div>
           </v-col>
@@ -95,8 +95,10 @@
       :headers="headers"
       :items="permissions"
       :items-per-page="10"
+      :no-data-text="$t('noDataText')"
       :footer-props="{
-        itemsPerPageOptions: [10, 20, 50]
+        itemsPerPageOptions: [10, 20, 50],
+        itemsPerPageText: this.$t('allDataTableText')
       }"
       :loading="loading"
       class="mt-4 rounded-lg"
@@ -108,10 +110,10 @@
       <template #top>
         <v-toolbar elevation="0">
           <v-toolbar-title class="d-flex justify-space-between w-full">
-            <div class="font-weight-medium">Permission</div>
+            <div class="font-weight-medium">{{ $t('permissionControl.dialog.permission') }}</div>
             <v-btn color="#7631FF" class="rounded-lg text-capitalize" dark @click="new_dialog = !new_dialog">
               <v-icon>mdi-plus</v-icon>
-              Permission
+              {{ $t('permissionControl.dialog.permission') }}
             </v-btn>
           </v-toolbar-title>
         </v-toolbar>
@@ -124,10 +126,10 @@
 
       <template #item.actions="{item}">
         <v-btn icon color="green" @click.stop="editItem(item)">
-          <v-img src="edit-active.svg" max-width="22"/>
+          <v-img src="/edit-active.svg" max-width="22"/>
         </v-btn>
         <v-btn icon color="red" @click.stop="getDeleteItem(item)">
-          <v-img src="delete.svg" max-width="27"/>
+          <v-img src="/delete.svg" max-width="27"/>
         </v-btn>
       </template>
       <template #item.status="{item}">
@@ -150,7 +152,7 @@
     <v-dialog v-model="new_dialog" max-width="600" ref="new_form">
       <v-card>
         <v-card-title class="w-full d-flex justify-space-between">
-          <div>New Permission</div>
+          <div>{{ $t('permissionControl.dialog.newPermission') }}</div>
           <v-btn @click="new_dialog = !new_dialog" icon>
             <v-icon color="#7631FF">mdi-close</v-icon>
           </v-btn>
@@ -160,36 +162,36 @@
             <v-row class="mt-4">
               <v-col cols="12">
                 <v-text-field
-                  label="Permission name"
+                  :label="$t('permissionControl.dialog.permissionName')"
                   filled
                   dense
                   v-model="new_permissionData.name"
                   color="#7631FF"
-                  placeholder="Enter permission name"
+                  :placeholder="$t('permissionControl.dialog.enterPermissionName')"
                   :rules="[formRules.required]"
                   validate-on-blur
                 />
               </v-col>
               <v-col cols="12">
                 <v-textarea
-                  label="Description"
+                  :label="$t('permissionControl.dialog.description')"
                   filled
                   dense
                   v-model="new_permissionData.description"
                   color="#7631FF"
-                  placeholder="Enter permission description"
+                  :placeholder="$t('permissionControl.dialog.enterPermissionDescription')"
                   :rules="[formRules.required]"
                   validate-on-blur
                 />
               </v-col>
               <v-col cols="12">
                 <v-text-field
-                  label="Permission path"
+                  :label="$t('permissionControl.dialog.permissionPath')"
                   filled
                   dense
                   v-model="new_permissionData.path"
                   color="#7631FF"
-                  placeholder="Enter permission path"
+                  :placeholder="$t('permissionControl.dialog.enterPermissionPath')"
                   :rules="[formRules.required]"
                   validate-on-blur
                 />
@@ -204,7 +206,7 @@
             color="#7631FF"
             width="163"
             @click="new_dialog = !new_dialog"
-          >cancel
+          >{{ $t('permissionControl.dialog.cancel') }}
           </v-btn>
           <v-btn
             class="text-capitalize rounded-lg font-weight-bold"
@@ -213,7 +215,7 @@
             width="163"
             @click="save"
           >
-            create
+            {{ $t('permissionControl.dialog.create') }}
           </v-btn>
         </v-card-actions>
       </v-card>
@@ -221,7 +223,7 @@
     <v-dialog v-model="edit_dialog" max-width="600">
       <v-card>
         <v-card-title class="w-full d-flex justify-space-between">
-          <div>Edit user</div>
+          <div>{{ $t('permissionControl.dialog.editUser') }}</div>
           <v-btn @click="edit_dialog = !edit_dialog" icon>
             <v-icon color="#7631FF">mdi-close</v-icon>
           </v-btn>
@@ -243,48 +245,48 @@
               </v-col>
               <v-col cols="12" md="6">
                 <v-select
-                  label="Permission name"
+                  :label="$t('permissionControl.dialog.status')"
                   filled
                   dense
                   :items="statusEnums"
                   v-model="edit_permission.status"
                   color="#7631FF"
                   append-icon="mdi-chevron-down"
-                  placeholder="Enter permission name"
+                  :placeholder="$t('permissionControl.dialog.selectStatus')"
                   validate-on-blur
                 />
               </v-col>
               <v-col cols="12">
                 <v-text-field
-                  label="Permission name"
+                  :label="$t('permissionControl.dialog.permissionName')"
                   filled
                   dense
                   v-model="edit_permission.name"
                   color="#7631FF"
-                  placeholder="Enter permission name"
+                  :placeholder="$t('permissionControl.dialog.enterPermissionName')"
                   validate-on-blur
                 />
               </v-col>
               <v-col cols="12">
                 <v-textarea
-                  label="Description"
+                  :label="$t('permissionControl.dialog.description')"
                   filled
                   dense
                   rows="1"
                   v-model="edit_permission.description"
                   color="#7631FF"
-                  placeholder="Enter permission description"
+                  :placeholder="$t('permissionControl.dialog.enterPermissionDescription')"
                   validate-on-blur
                 />
               </v-col>
               <v-col cols="12">
                 <v-text-field
-                  label="Permission path"
+                  :label="$t('permissionControl.dialog.permissionPath')"
                   filled
                   dense
                   v-model="edit_permission.path"
                   color="#7631FF"
-                  placeholder="Enter permission path"
+                  :placeholder="$t('permissionControl.dialog.enterPermissionPath')"
                   validate-on-blur
                 />
               </v-col>
@@ -298,7 +300,7 @@
             color="#7631FF"
             width="163"
             @click="edit_dialog = !edit_dialog"
-          >cancel
+          >{{ $t('permissionControl.dialog.cancel') }}
           </v-btn>
           <v-btn
             class="text-capitalize rounded-lg font-weight-bold"
@@ -307,7 +309,7 @@
             width="163"
             @click="updatePermission"
           >
-            create
+            {{ $t('permissionControl.dialog.create') }}
           </v-btn>
         </v-card-actions>
       </v-card>
@@ -317,9 +319,9 @@
         <div class="d-flex justify-center mb-2">
           <v-img src="/error-icon.svg" max-width="40"/>
         </div>
-        <v-card-title class="d-flex justify-center">Delete Permission</v-card-title>
+        <v-card-title class="d-flex justify-center">{{ $t('permissionControl.dialog.deleteDialog') }}</v-card-title>
         <v-card-text>
-          Are you sure you want to Delete this permission?
+          {{ $t('permissionControl.dialog.deleteText') }}
         </v-card-text>
         <v-card-actions class="px-16">
           <v-btn
@@ -329,7 +331,7 @@
             width="140"
             @click.stop="delete_dialog = false"
           >
-            cancel
+            {{ $t('permissionControl.dialog.cancel') }}
           </v-btn>
           <v-spacer/>
           <v-btn
@@ -340,7 +342,7 @@
             dark
             @click="deleteData"
           >
-            delete
+            {{ $t('permissionControl.dialog.delete') }}
           </v-btn>
         </v-card-actions>
       </v-card>
@@ -360,13 +362,13 @@ export default {
       new_dialog: false,
       headers: [
         {text: 'ID', align: 'start', sortable: false, value: 'id'},
-        {text: 'Permission name', value: 'name'},
-        {text: 'Description', value: 'description'},
-        {text: 'Path', value: 'path'},
-        {text: 'Status', value: 'status', width: '150'},
-        {text: 'Updated', value: 'updatedAt', align: 'center'},
-        {text: 'Created', value: 'createdAt', align: 'center'},
-        {text: 'Actions', value: 'actions', width: '180', align: 'center', sortable: false},
+        {text: this.$t('permissionControl.table.permissionName'), value: 'name'},
+        {text: this.$t('permissionControl.table.description'), value: 'description'},
+        {text: this.$t('permissionControl.table.path'), value: 'path'},
+        {text: this.$t('permissionControl.table.status'), value: 'status', width: '150'},
+        {text: this.$t('permissionControl.table.updated'), value: 'updatedAt', align: 'center'},
+        {text: this.$t('permissionControl.table.created'), value: 'createdAt', align: 'center'},
+        {text: this.$t('permissionControl.table.actions'), value: 'actions', width: '180', align: 'center', sortable: false},
       ],
       options: {},
       filter_search: {
@@ -464,7 +466,7 @@ export default {
     },
   },
   mounted() {
-    this.$store.commit('setPageTitle', 'Permission');
+    this.$store.commit('setPageTitle', this.$t('permissionControl.dialog.permission'));
     this.$store.dispatch('permission/getPermission', {page: this.current_page, size: this.itemPerPage});
   }
 }
