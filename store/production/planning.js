229c1a1c7b2e93f58a3e1542906b88ddc3f9d9bd
Abbos Kahamidullaev <khamidullaev1998@gmail.com>
2023-04-11 11:54:51 +0500

    add get req model info

--- store/production/planning.js
+++ store/production/planning.js
@@ -1,13 +1,18 @@
 export const state = () => ({
-  planningList: []
+  planningList: [],
+  modelInfo: {}
 });
 export const getters = {
   planningList: state => state.planningList.content,
-  totalElements: state => state.planningList.totalElements
+  totalElements: state => state.planningList.totalElements,
+  modelInfo: state => state.modelInfo
 }
 export const mutations = {
   setPlanningList(state, planning) {
     state.planningList = planning;
+  },
+  setModelInfo(state, info) {
+    state.modelInfo = info;
   }
 }
 export const actions = {
@@ -21,5 +26,13 @@ export const actions = {
       .then(res => {
         commit('setPlanningList', res.data);
       }).catch(({response}) => console.log(response))
+  },
+  getModelInfo({commit}, id) {
+    this.$axios.$get(`/api/v1/production/model-order-info?modelId=${id}`)
+      .then(res => {
+        commit('setModelInfo', res.data)
+      }).catch(({response}) => {
+        this.$toast.error(response.data.message);
+    })
   }
 }
