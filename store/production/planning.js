5214aeb0bce14ddd54d6d99d85de790719859b73
Abbos Kahamidullaev <khamidullaev1998@gmail.com>
2023-04-11 23:41:01 +0500

    end off create prod planning

--- store/production/planning.js
+++ store/production/planning.js
@@ -4,16 +4,19 @@ export const state = () => ({
   processList: {},
   workshopList: [],
   colorsList: {},
-  productionId: ''
+  productionId: '',
+  processingList: {}
 });
 export const getters = {
-  planningList: state => state.planningList?.content.filter(obj => Object.keys(obj).length !== 0),
+  planningList: state => state.planningList.content,
   totalElements: state => state.planningList.totalElements,
   modelInfo: state => state.modelInfo,
   processList: state => state.processList.content,
   workshopList: state => state.workshopList,
   colorsList: state => state.colorsList.content,
-  productionId: state => state.productionId
+  productionId: state => state.productionId,
+  processingList: state => state.processingList.content,
+  processingTotalElements: state => state.processingList.totalElements
 }
 export const mutations = {
   setPlanningList(state, planning) {
@@ -30,6 +33,12 @@ export const mutations = {
   },
   setColorsList(state, color) {
     state.colorsList = color;
+  },
+  setProductionId(state, id) {
+    state.productionId = id;
+  },
+  setProcessingList(state, item) {
+    state.processingList = item;
   }
 }
 export const actions = {
@@ -80,17 +89,36 @@ export const actions = {
         commit('setColorsList', res.data)
       }).catch(({response}) => console.log(response))
   },
-  createProcessPlanning({commit}, data) {
+  createProcessPlanning({commit, dispatch}, data) {
     this.$axios.$post('/api/v1/production/create', data)
       .then(res => {
+        commit('setProductionId', res.data.id);
+        dispatch('getProcessingList', {
+          id: res.data.id,
+          page: 0, size: 10
+        });
         this.$toast.success(res.message)
       }).catch(({response}) => console.log(response));
   },
-  createProcessing({commit}, data) {
-    console.log(data);
-    // this.$axios.$post('/api/v1/process-planning/create', data)
-    //   .then(res => {
-    //     console.log(res);
-    //   }).catch(({response}) => console.log(response));
+  createProcessing({dispatch}, data) {
+    this.$axios.$post('/api/v1/process-planning/create', data)
+      .then(res => {
+        dispatch('getProcessingList', {
+          id: data.productionId,
+          page: 0, size: 10
+        })
+        this.$toast.success(res.message);
+      }).catch(({response}) => console.log(response));
+  },
+  getProcessingList({commit}, {id, page = 0, size= 10}) {
+    const body = {
+      filters: [],
+      sorts: [],
+      page, size,
+    }
+    this.$axios.$put(`/api/v1/process-planning/list?productionId=${id}`, body)
+      .then(res => {
+        commit('setProcessingList', res.data);
+      }).catch(({response}) => console.log(response));
   }
 }
