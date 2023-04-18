9a6b2fa661ee2c6b8e8a1edfd899a27977a3fbdf
Abbos Kahamidullaev <khamidullaev1998@gmail.com>
2023-04-18 21:32:26 +0500

    end off prod planning CRUD

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ import colors from 'vuetify/es5/util/colors'
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'ATP - Mytex',
+    title: 'ATP - textile',
     htmlAttrs: {
       lang: 'en'
     },
