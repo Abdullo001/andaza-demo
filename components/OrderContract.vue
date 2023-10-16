9235e39e332c6f6339e7d71204437d41a54e8ade
Abdullo <abdullofayzullayev68@gmail.com>
2023-10-16 15:48:23 +0300

    fix bug fabricOrdering

--- components/OrderContract.vue
+++ components/OrderContract.vue
@@ -203,6 +203,210 @@
         </v-card-actions>
       </v-card>
     </v-dialog>
+
+    <v-dialog max-width="600" v-model="edit_dialog">
+      <v-card>
+        <v-card-title class="w-full d-flex justify-space-between mb-6">
+          <div class="title text-capitalize">add contract</div>
+          <v-btn icon color="#7631FF" @click="edit_dialog = false">
+            <v-icon>mdi-close</v-icon>
+          </v-btn>
+        </v-card-title>
+        <v-card-text>
+          <v-form ref="new_validate" lazy-validation>
+            <v-row>
+              <v-col cols="6">
+                <div class="label text-capitalize mb-2">Contract/SA No.</div>
+                <v-text-field
+                  outlined
+                  single-line
+                  placeholder="Contract/SA No"
+                  v-model="selectItem.contractNumber"
+                  color="#7631FF"
+                  dense
+                  class="rounded-lg base"
+                  height="44"
+                  hide-details
+                />
+              </v-col>
+              <v-col cols="6">
+                <div class="label">Date of contract</div>
+                <div style="height: 40px !important">
+                  <el-date-picker
+                    v-model="selectItem.contractDate"
+                    type="datetime"
+                    style="width: 100%; height: 100%"
+                    placeholder="dd.MM.yyyy HH:mm:ss"
+                    :picker-options="pickerShortcuts"
+                    value-format="dd.MM.yyyy HH:mm:ss"
+                    class="base_picker"
+                  >
+                  </el-date-picker>
+                </div>
+              </v-col>
+              <v-col cols="6">
+                <div class="label">Deadline of contract</div>
+                <div style="height: 40px !important">
+                  <el-date-picker
+                    v-model="selectItem.deadline"
+                    type="datetime"
+                    style="width: 100%; height: 100%"
+                    placeholder="dd.MM.yyyy HH:mm:ss"
+                    :picker-options="pickerShortcuts"
+                    value-format="dd.MM.yyyy HH:mm:ss"
+                    class="base_picker"
+                  >
+                  </el-date-picker>
+                </div>
+              </v-col>
+              <v-col cols="6">
+                <div class="label text-capitalize mb-2">price</div>
+                <div class="d-flex align-center">
+                  <v-text-field
+                    v-model="selectItem.sum"
+                    placeholder="0.00"
+                    outlined
+                    hide-details
+                    height="44"
+                    class="rounded-lg base rounded-l-lg rounded-r-0"
+                    validate-on-blur
+                    dense
+                    color="#7631FF"
+                  />
+                  <v-select
+                    :items="currency"
+                    v-model="selectItem.sumCurrency"
+                    style="max-width: 100px"
+                    dense
+                    outlined
+                    hide-details
+                    height="44"
+                    class="rounded-lg base rounded-r-lg rounded-l-0"
+                    validate-on-blur
+                    append-icon="mdi-chevron-down"
+                    color="#7631FF"
+                  />
+                </div>
+              </v-col>
+              <v-col cols="6">
+                <div class="label text-capitalize mb-2">Delivery condition</div>
+                <v-select
+                  outlined
+                  :items="conditionList"
+                  v-model="selectItem.condition"
+                  single-line
+                  placeholder="Delivery condition"
+                  dense
+                  append-icon="mdi-chevron-down"
+                  color="#7631FF"
+                  class="rounded-lg base"
+                  height="44"
+                  hide-details
+                />
+              </v-col>
+              <v-col cols="12">
+                <div class="text-body-1 font-weight-medium mb-3">
+                  {{ $t("partners.dialog.uploadContract") }}
+                </div>
+                <v-card style="border: #a5a7ad dashed 1px" elevation="0">
+                  <v-card-title>
+                    <input
+                      ref="uploader"
+                      class="d-none"
+                      type="file"
+                      @change="onChangeFile"
+                    />
+                    <v-btn
+                      @click="clickImportFile"
+                      elevation="0"
+                      style="background-color: #f1ebfe; color: #7631ff"
+                      :disabled="docList.length !== 0"
+                      class="font-weight-bold mr-3 text-capitalize"
+                    >
+                      {{ $t("partners.dialog.uploadFiles") }}
+                    </v-btn>
+                  </v-card-title>
+                  <v-card-text>
+                    <div v-for="(item, idx) in docList" :key="`imade_${idx}`">
+                      <div class="d-flex justify-space-between align-center">
+                        <p class="font-weight-bold">
+                          <v-icon class="mr-3" color="green">
+                            mdi-check
+                          </v-icon>
+                          {{ item.name }}
+                        </p>
+                        <p>
+                          <v-btn icon @click="removeImage(item.size)">
+                            <v-icon> mdi-close</v-icon>
+                          </v-btn>
+                        </p>
+                      </div>
+                      <v-divider />
+                    </div>
+                  </v-card-text>
+                </v-card>
+              </v-col>
+            </v-row>
+          </v-form>
+        </v-card-text>
+        <v-card-actions class="d-flex justify-center mt-8">
+          <v-btn
+            class="rounded-lg font-weight-bold"
+            color="#7631FF"
+            width="130"
+            outlined
+            height="44"
+            style="border-width: 2px"
+            @click="edit_dialog = false"
+            >cancel
+          </v-btn>
+          <v-btn
+            class="rounded-lg white--text ml-6"
+            color="#7631FF"
+            width="130"
+            height="44"
+            @click="save"
+            >save
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+
+    <v-dialog v-model="delete_dialog" max-width="500">
+      <v-card class="pa-4 text-center">
+        <div class="d-flex justify-center mb-2">
+          <v-img src="/error-icon.svg" max-width="40" />
+        </div>
+        <v-card-title class="d-flex justify-center"
+          >Delete contract
+        </v-card-title>
+        <v-card-text>
+          Are you sure you want to Delete contract?
+        </v-card-text>
+        <v-card-actions class="px-16">
+          <v-btn
+            outlined
+            class="rounded-lg text-capitalize font-weight-bold"
+            color="#777C85"
+            width="140"
+            @click.stop="delete_dialog = false"
+          >
+            cancel
+          </v-btn>
+          <v-spacer />
+          <v-btn
+            class="rounded-lg text-capitalize font-weight-bold"
+            color="#FF4E4F"
+            width="140"
+            elevation="0"
+            dark
+            @click="deleteFunc"
+          >
+            delete
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </div>
 </template>
 <script>
@@ -212,15 +416,11 @@ export default {
   data() {
     return {
       newDialog: false,
+      edit_dialog:false,
+      delete_dialog:false,
       currency: ["USD", "RUB", "UZS"],
       conditionList: ["CIF", "CIP", "CPT", "EXW", "FCA", "FOB"],
       headers: [
-        {
-          text: "Client name",
-          sortable: false,
-          align: "center",
-          value: "client",
-        },
         { text: "Contract/SA No.", sortable: false, align: "center", value: "contractNumber" },
         { text: "Contract/SA date", sortable: false, align: "center", value: "contractDate" },
         { text: "Deadline", sortable: false, align: "center", value: "deadline" },
@@ -239,6 +439,7 @@ export default {
         condition: "",
       },
       docList: [],
+      selectItem:{},
     };
   },
 
@@ -258,6 +459,8 @@ export default {
     ...mapActions({
       createContract: "orderContract/createContract",
       getContractList: "orderContract/getContractList",
+      deleteContract: "orderContract/deleteContract",
+      getOneContract: "orderContract/getOneContract",
     }),
 
     onChangeFile(e) {
@@ -294,12 +497,27 @@ export default {
         formData.append("file",file)
       }
       this.createContract({data:formData,orderId})
+      this.newDialog=false
     },
 
-    openEditDialog(item) {},
-    currentPrint(item) {
-      
+    openEditDialog(item) {
+      this.edit_dialog=true
+      this.selectItem={...item}
+      this.getOneContract(item.id)
     },
+
+
+    
+    currentPrint(item) {
+      this.delete_dialog=true
+      this.selectItem={...item}
+    },
+
+    deleteFunc(){
+      const orderId=this.$route.params.id
+      this.deleteContract({id:this.selectItem.id,orderId})
+      this.delete_dialog=false
+    }
   },
 
   mounted(){
