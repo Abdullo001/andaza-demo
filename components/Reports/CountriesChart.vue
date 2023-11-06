c207c82057ce6d6b6b7095d1cd2fe1f41bf74207
Abbos Kahamidullaev <khamidullaev1998@gmail.com>
2023-11-06 19:47:36 +0500

    reports add client, country, horizontal charts

---
+++ components/Reports/CountriesChart.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <v-card elevation="0" rounded="lg">
+      <v-card-title class="d-flex align-center justify-space-between">
+        <div>Prefinance creators</div>
+      </v-card-title>
+      <v-card-text>
+        <div>
+          <DoughnutChart
+            :chart-options="doughnut_options"
+            :chart-data="doughnut"
+            chart-id="Doughnut-char"
+            :label="doughnut.labels"
+            :width="doughnut_options.width"
+            :height="doughnut_options.height"
+          />
+        </div>
+      </v-card-text>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CountriesChartComponent',
+  data() {
+    return {
+      doughnut: {
+        labels: ['Russian', 'Belorus', 'Kazakhstan', 'Lithuania'],
+        datasets: [
+          {
+            backgroundColor: [
+              '#7631FF',
+              'rgb(255, 99, 132)',
+              'rgb(255, 205, 86)',
+              '#10BF41'
+            ],
+            data: [57, 7, 22, 15]
+          },
+        ],
+      },
+      doughnut_options: {
+        maintainAspectRatio: false,
+        responsive: true,
+        width: 400,
+        height: 369
+      },
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- pages/report.vue
+++ pages/report.vue
@@ -1,38 +1,38 @@
 <template>
-  <div>
+  <div class="pb-10">
     <v-sheet
       class="mt-5 py-3 px-5 d-flex align-center"
       rounded="lg"
     >
       Year
       <v-select
-        :items="years"
         v-model="filters.year"
+        :items="years"
         append-icon="mdi-chevron-down"
-        solo
-        class="elevation-0 ml-2"
         background-color="#F3F4F5"
-        hide-details
-        style="max-width: 100px;"
+        class="elevation-0 ml-2"
         dense
+        hide-details
+        solo
+        style="max-width: 100px;"
       />
       <v-checkbox
-        label="Amount"
-        color="#7631FF"
-        class="ml-11"
         v-model="filters.amount"
+        class="ml-11"
+        color="#7631FF"
+        label="Amount"
       />
       <v-checkbox
-        label="Quantity"
-        color="#7631FF"
-        class="ml-11"
         v-model="filters.quantity"
+        class="ml-11"
+        color="#7631FF"
+        label="Quantity"
       />
       <v-checkbox
-        label="Percentage"
-        color="#7631FF"
-        class="ml-11"
         v-model="filters.percentage"
+        class="ml-11"
+        color="#7631FF"
+        label="Percentage"
       />
     </v-sheet>
     <v-row class="mt-5">
@@ -45,6 +45,15 @@
       <v-col cols="12" lg="4">
         <LineChartComponent/>
       </v-col>
+      <v-col cols="12" lg="4">
+        <HorizontalChartComponent/>
+      </v-col>
+      <v-col cols="12" lg="4">
+        <CountriesChartComponent/>
+      </v-col>
+      <v-col cols="12" lg="4">
+        <ClientsChartComponent/>
+      </v-col>
     </v-row>
   </div>
 </template>
@@ -56,14 +65,17 @@ export default {
     BarChartComponent: () => import("@/components/Reports/BarChart.vue"),
     DoughnutChartComponent: () => import("@/components/Reports/DoughnutChart.vue"),
     LineChartComponent: () => import("@/components/Reports/LineChart.vue"),
+    HorizontalChartComponent: () => import("@/components/Reports/HorizontalChart.vue"),
+    CountriesChartComponent: () => import("@/components/Reports/CountriesChart.vue"),
+    ClientsChartComponent: () => import("@/components/Reports/ClientsChart.vue"),
   },
   data() {
     return {
       chartData: [
-        { label: 'Category 1', value: 25, backgroundColor: 'red' },
-        { label: 'Category 2', value: 40, backgroundColor: 'blue' },
-        { label: 'Category 3', value: 20, backgroundColor: 'green' },
-        { label: 'Category 4', value: 15, backgroundColor: 'orange' },
+        {label: 'Category 1', value: 25, backgroundColor: 'red'},
+        {label: 'Category 2', value: 40, backgroundColor: 'blue'},
+        {label: 'Category 3', value: 20, backgroundColor: 'green'},
+        {label: 'Category 4', value: 15, backgroundColor: 'orange'},
       ],
       years: [2023, 2022, 2021],
       filters: {
@@ -80,38 +92,44 @@ export default {
 
 <style lang="scss">
 .v-text-field.v-text-field--solo:not(.v-text-field--solo-flat) > .v-input__control > .v-input__slot {
-  box-shadow: none!important;
+  box-shadow: none !important;
 }
+
 .color1 {
   width: 26px;
   height: 15px;
   border-radius: 5px;
   background: #EEF0FA;
   margin-right: 8px;
-  &+div {
+
+  & + div {
     color: #000;
     font-size: 14px;
     font-style: normal;
     line-height: 10px;
     text-transform: capitalize;
-    &>span {
+
+    & > span {
       font-weight: 700;
     }
   }
 }
+
 .color2 {
   width: 26px;
   height: 15px;
   border-radius: 5px;
   background: #7631FF;
   margin-right: 8px;
-  &+div {
+
+  & + div {
     color: #000;
     font-size: 14px;
     font-style: normal;
     line-height: 10px;
     text-transform: capitalize;
-    &>span {
+
+    & > span {
       font-weight: 700;
     }
   }
