038322840fd4d7c608492b7ac987367ebe732e11
Abdullo Fayzullayev <abdullofayzullayev68@gmail.com>
2024-05-13 17:28:03 +0500

    bug fix add notification button

--- pages/notification/index.vue
+++ pages/notification/index.vue
@@ -164,7 +164,7 @@ export default {
       getNotificationList:"notification/getNotificationList",
     }),
     addNotification(){
-
+      this.$router.push("/notification/add-notification")
     },
     async page(value) {
       this.current_page = value - 1;
