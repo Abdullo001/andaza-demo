a482cddb376c6c925dfd6b91fb13b357dcdbdb4e
Abdulfattoh <abdulfattokh55@gmail.com>
2024-12-29 13:52:02 +0500

    model category operations

--- store/model.js
+++ store/model.js
@@ -1,11 +1,15 @@
 export const state = () => ({
   loading: true,
   modelGroupList: [],
+  selectedModelOperations: null,
+  selectedModel: null,
 })
 export const getters = {
   loading: state => state.loading,
   modelGroupList: state => state.modelGroupList.items,
   modelTotalElements: state=> state.modelGroupList.totalElements,
+  selectedModelOperations: state => state.selectedModelOperations,
+  selectedModel: state => state.selectedModel,
 }
 export const mutations = {
   setLoading(state, loadings) {
@@ -13,6 +17,12 @@ export const mutations = {
   },
   setModelGroupList(state, data) {
     state.modelGroupList = data
+  },
+  setSelectedModelOperations(state, item) {
+    state.selectedModelOperations = item
+  },
+  setSelectedModel(state, item) {
+    state.selectedModel = item
   },
 }
 export const actions = {
@@ -58,6 +68,52 @@ export const actions = {
         console.log(response);
         commit('setLoading', false);
       })
-  }
+  },
+  async getSelectedModelOperations({ commit }, id) {
+    await this.$axios.get(`/api/v1/model-category-operations?modelCategoryId=${id}`)
+      .then((res) => {
+        commit("setSelectedModelOperations", res.data.data);
+      })
+      .catch(({ response }) => {
+        console.log(response);
+      });
+  },
+  async getSelectedModel({ commit }, id) {
+    await this.$axios.get(`/api/v1/model-category/${id}`)
+      .then((res) => {
+        commit("setSelectedModel", res.data.data);
+      })
+      .catch(({ response }) => {
+        console.log(response);
+      });
+  },
+  async resetOperations({ commit }, id) {
+    await this.$axios.post(`/api/v1/model-category-operations/reset?modelCategoryId=${id}`)
+      .then((res) => {
+        commit("setSelectedModelOperations", res.data.data);
+      })
+      .catch(({ response }) => {
+        console.log(response);
+      });
+  },
+  async deleteOperations({ dispatch }, {id, data}) {
+    await this.$axios.delete(`/api/v1/model-category-operations?modelCategoryId=${id}&operationIds=${data}`)
+      .then((res) => {
+        dispatch("getSelectedModelOperations", id);
+      })
+      .catch(({ response }) => {
+        console.log(response);
+      });
+  },
+  async updateOperations({ commit }, data) {
+    await this.$axios.post(`/api/v1/model-category-operations`, data)
+      .then((res) => {
+        commit("setSelectedModelOperations", res.data.data);
+        this.$toast.success(res.data.code);
+      })
+      .catch(({ response }) => {
+        console.log(response);
+      });
+  },
 }
 
