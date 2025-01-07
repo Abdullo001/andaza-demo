02b98c07555c211830f0674d34cceeeb19644208
Abdullo Fayzullayev <abdullofayzullayev68@gmail.com>
2025-01-07 15:15:58 +0500

    fix: remove pantone code in size distribution tab

--- components/ColorSizeDistribution.vue
+++ components/ColorSizeDistribution.vue
@@ -304,7 +304,7 @@
                       />
                     </div>
                   </v-col>
-                  <v-col cols="6" v-if="!item.colorCode">
+                  <v-col cols="6" v-if="item.bodyPartId">
                     <div>
                       <div class="label">{{ $t('colorsBox.table.pantoneCode') }}</div>
                       <div class="d-flex align-center">
